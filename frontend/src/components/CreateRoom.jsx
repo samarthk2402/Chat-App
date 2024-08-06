ed6eda385e76fef0eab5afadcd208fc03e8654dc
Samarth Kumar <samarthk2402@gmail.com>
2024-08-06 13:57:04 +0530

    Added page state to home page

---
+++ frontend/src/components/CreateRoom.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const CreateRoom = () => {
+  return (
+    <Card style={{ maxWidth: "200px", padding: "20px" }}>
+      <h3>Create a room</h3>
+    </Card>
+  );
+};
+
+export default CreateRoom;
